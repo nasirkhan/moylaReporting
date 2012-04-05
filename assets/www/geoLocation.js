c14824e7d640f50312c6ffe02bfb6cbc104e7525
sheikh303 <sheikh303@gmail.com>
2012-04-05 19:19:54 +0600

    server comm.

--- assets/www/geoLocation.js
+++ assets/www/geoLocation.js
@@ -11,15 +11,16 @@
     // onSuccess Geolocation
     //
     function onSuccess(position) {
-        var element = document.getElementById('geolocation');
-        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+//        var element = document.getElementById('geolocation');
+        alert('Latitude: '           + position.coords.latitude              + '<br />' +
                             'Longitude: '          + position.coords.longitude             + '<br />' +
                             'Altitude: '           + position.coords.altitude              + '<br />' +
                             'Accuracy: '           + position.coords.accuracy              + '<br />' +
                             'Altitude Accuracy: '  + position.coords.altitudeAccuracy      + '<br />' +
                             'Heading: '            + position.coords.heading               + '<br />' +
                             'Speed: '              + position.coords.speed                 + '<br />' +
-                            'Timestamp: '          + new Date(position.timestamp)          + '<br />';
+                            'Timestamp: '          + new Date(position.timestamp)          + '<br />'
+                        );
     }
 
     // onError Callback receives a PositionError object
--- src/com/awlad/phonegap/AwladGapActivity.java
+++ src/com/awlad/phonegap/AwladGapActivity.java
@@ -8,6 +8,6 @@ public class AwladGapActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		super.loadUrl("file:///android_asset/www/index3.html");
+		super.loadUrl("file:///android_asset/www/uploadpic.html");
 	}
 }
