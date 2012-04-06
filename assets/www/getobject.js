dd639bd6593612192f7dbf21845016c39bfa199c
nasir8891 <nasir8891@gmail.com>
2012-04-06 14:27:50 +0600

    geo test

--- assets/www/getobject.js
+++ assets/www/getobject.js
@@ -1,24 +1,24 @@
 function getobjectID(imageURI){
 	alert("Getting ObjectID");
     //alert(latitude+" "+longtitude);
-	$.ajax({
-        type: 'POST',
-        data: '{"NAME":"faiyaz","PREFIX":"Mr","INITIALS":"SFM","EMAIL":"faiyaz@asd.com","PHONE_NUMBER":"013-1234567","GENDER":"V","BIRTHYEAR":"1984","INCIDENT_CODE":"2","REMARK":"test using phonegap","MUNICIPALITY":"Tilburg","GPS_LATITUDE":"'+latitude+'","GPS_LONGITUDE":"'+longtitude+'"}',
-        url: 'http://services.terbit.nl:2357/TWMSService/Report',
-        cache: false,
-        contentType: 'application/json;charset=utf-8',
-		dataType: 'json',
-        success: function(data){
-            objectId=data;
-            alert(objectId);
-	    uploadPhoto(objectId,imageURI);
-            
-        },
-        error: function(data){
-            alert(data);
-            alert('There was an error adding your comment');
-        }
-    });
- 
+//	$.ajax({
+//        type: 'POST',
+//        data: '{"NAME":"faiyaz","PREFIX":"Mr","INITIALS":"SFM","EMAIL":"faiyaz@asd.com","PHONE_NUMBER":"013-1234567","GENDER":"V","BIRTHYEAR":"1984","INCIDENT_CODE":"2","REMARK":"test using phonegap","MUNICIPALITY":"Tilburg","GPS_LATITUDE":"23,1232212","GPS_LONGITUDE":"12,43433443"}',
+//        url: 'http://services.terbit.nl:2357/TWMSService/Report',
+//        cache: false,
+//        contentType: 'application/json;charset=utf-8',
+//	dataType: 'json',
+//        success: function(data){
+//            objectId=data;
+//            alert(objectId);
+//	    uploadPhoto(objectId);
+//            
+//        },
+//        error: function(data){
+//            alert(data);
+//            alert('There was an error adding your comment');
+//        }
+//    });
+    uploadPhoto('718');
     return false;
 }
